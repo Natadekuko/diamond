22f0a6dafa28ab3ff681847c919e2edba946deb6
Natadekuko <156817585+Natadekuko@users.noreply.github.com>
2024-02-28 00:33:42 +0800

    Add files via upload

---
+++ profiles.js
@@ -0,0 +1,302 @@
+const buttons = document.querySelectorAll(".button");
+
+for (let button of buttons) {
+    console.log('reached');
+    button.addEventListener("click", e => {
+        e.preventDefault();
+        button.classList.add("animate");
+
+        setTimeout(() => {
+            button.classList.remove("animate");
+            window.location.href = button.getAttribute("href");
+        }, 600);
+    });
+}
+
+function changeImage1() {
+    var img = document.getElementById('b1');
+    img.style.opacity = '0';
+    setTimeout(function () {
+        img.src = 'ab1.png';
+        img.style.opacity = '1';
+    }, 500);
+}
+
+function changeImage2() {
+    var img = document.getElementById('b2');
+    img.style.opacity = '0';
+    setTimeout(function () {
+        img.src = 'ab2.png';
+        img.style.opacity = '1';
+    }, 500);
+}
+
+function changeImage3() {
+    var img = document.getElementById('b3');
+    img.style.opacity = '0';
+    setTimeout(function () {
+        img.src = 'ab3.png';
+        img.style.opacity = '1';
+    }, 500);
+}
+
+function changeImage4() {
+    var img = document.getElementById('b4');
+    img.style.opacity = '0';
+    setTimeout(function () {
+        img.src = 'ab4.png';
+        img.style.opacity = '1';
+    }, 500);
+}
+
+function changeImage5() {
+    var img = document.getElementById('b5');
+    img.style.opacity = '0';
+    setTimeout(function () {
+        img.src = 'ab5.png';
+        img.style.opacity = '1';
+    }, 500);
+}
+
+function changeImage6() {
+    var img = document.getElementById('b6');
+    img.style.opacity = '0';
+    setTimeout(function () {
+        img.src = 'ab6.png';
+        img.style.opacity = '1';
+    }, 500);
+}
+
+function changeImage7() {
+    var img = document.getElementById('b7');
+    img.style.opacity = '0';
+    setTimeout(function () {
+        img.src = 'ab7.png';
+        img.style.opacity = '1';
+    }, 500);
+}
+
+function changeImage8() {
+    var img = document.getElementById('b8');
+    img.style.opacity = '0';
+    setTimeout(function () {
+        img.src = 'ab8.png';
+        img.style.opacity = '1';
+    }, 500);
+}
+
+function changeImage9() {
+    var img = document.getElementById('b9');
+    img.style.opacity = '0';
+    setTimeout(function () {
+        img.src = 'ab9.png';
+        img.style.opacity = '1';
+    }, 500);
+}
+
+function changeImage10() {
+    var img = document.getElementById('b10');
+    img.style.opacity = '0';
+    setTimeout(function () {
+        img.src = 'ab10.png';
+        img.style.opacity = '1';
+    }, 500);
+}
+
+function changeImage11() {
+    var img = document.getElementById('b11');
+    img.style.opacity = '0';
+    setTimeout(function () {
+        img.src = 'ab11.png';
+        img.style.opacity = '1';
+    }, 500);
+}
+
+function changeImage12() {
+    var img = document.getElementById('b12');
+    img.style.opacity = '0';
+    setTimeout(function () {
+        img.src = 'ab12.png';
+        img.style.opacity = '1';
+    }, 500);
+}
+
+function changeImage13() {
+    var img = document.getElementById('b13');
+    img.style.opacity = '0';
+    setTimeout(function () {
+        img.src = 'ab13.png';
+        img.style.opacity = '1';
+    }, 500);
+}
+
+function changeImage14() {
+    var img = document.getElementById('b14');
+    img.style.opacity = '0';
+    setTimeout(function () {
+        img.src = 'ab14.png';
+        img.style.opacity = '1';
+    }, 500);
+}
+
+function changeImage15() {
+    var img = document.getElementById('b15');
+    img.style.opacity = '0';
+    setTimeout(function () {
+        img.src = 'ab15.png';
+        img.style.opacity = '1';
+    }, 500);
+}
+
+function changeImage16() {
+    var img = document.getElementById('b16');
+    img.style.opacity = '0';
+    setTimeout(function () {
+        img.src = 'ab16.png';
+        img.style.opacity = '1';
+    }, 500);
+}
+
+function changeImage17() {
+    var img = document.getElementById('b17');
+    img.style.opacity = '0';
+    setTimeout(function () {
+        img.src = 'ab17.png';
+        img.style.opacity = '1';
+    }, 500);
+}
+
+function changeImage18() {
+    var img = document.getElementById('b18');
+    img.style.opacity = '0';
+    setTimeout(function () {
+        img.src = 'ab18.png';
+        img.style.opacity = '1';
+    }, 500);
+}
+
+function changeImage19() {
+    var img = document.getElementById('b19');
+    img.style.opacity = '0';
+    setTimeout(function () {
+        img.src = 'ab19.png';
+        img.style.opacity = '1';
+    }, 500);
+}
+
+function changeImage20() {
+    var img = document.getElementById('b20');
+    img.style.opacity = '0';
+    setTimeout(function () {
+        img.src = 'ab20.png';
+        img.style.opacity = '1';
+    }, 500);
+}
+
+function changeImage21() {
+    var img = document.getElementById('b21');
+    img.style.opacity = '0';
+    setTimeout(function () {
+        img.src = 'ab21.png';
+        img.style.opacity = '1';
+    }, 500);
+}
+
+function changeImage22() {
+    var img = document.getElementById('b22');
+    img.style.opacity = '0';
+    setTimeout(function () {
+        img.src = 'ab22.png';
+        img.style.opacity = '1';
+    }, 500);
+}
+
+function changeImage23() {
+    var img = document.getElementById('b23');
+    img.style.opacity = '0';
+    setTimeout(function () {
+        img.src = 'ab23.png';
+        img.style.opacity = '1';
+    }, 500);
+}
+
+function changeImage24() {
+    var img = document.getElementById('b24');
+    img.style.opacity = '0';
+    setTimeout(function () {
+        img.src = 'ab24.png';
+        img.style.opacity = '1';
+    }, 500);
+}
+
+function changeImage25() {
+    var img = document.getElementById('b25');
+    img.style.opacity = '0';
+    setTimeout(function () {
+        img.src = 'ab25.png';
+        img.style.opacity = '1';
+    }, 500);
+}
+
+function changeImage26() {
+    var img = document.getElementById('b26');
+    img.style.opacity = '0';
+    setTimeout(function () {
+        img.src = 'ab26.png';
+        img.style.opacity = '1';
+    }, 500);
+}
+
+function changeImage27() {
+    var img = document.getElementById('b27');
+    img.style.opacity = '0';
+    setTimeout(function () {
+        img.src = 'ab27.png';
+        img.style.opacity = '1';
+    }, 500);
+}
+
+function changeImage28() {
+    var img = document.getElementById('b28');
+    img.style.opacity = '0';
+    setTimeout(function () {
+        img.src = 'ab28.png';
+        img.style.opacity = '1';
+    }, 500);
+}
+
+function changeImage29() {
+    var img = document.getElementById('b29');
+    img.style.opacity = '0';
+    setTimeout(function () {
+        img.src = 'ab29.png';
+        img.style.opacity = '1';
+    }, 500);
+}
+
+function changeImage30() {
+    var img = document.getElementById('b30');
+    img.style.opacity = '0';
+    setTimeout(function () {
+        img.src = 'ab30.png';
+        img.style.opacity = '1';
+    }, 500);
+}
+
+function changeImage31() {
+    var img = document.getElementById('b31');
+    img.style.opacity = '0';
+    setTimeout(function () {
+        img.src = 'ab31.png';
+        img.style.opacity = '1';
+    }, 500);
+}
+
+function changeImage32() {
+    var img = document.getElementById('b32');
+    img.style.opacity = '0';
+    setTimeout(function () {
+        img.src = 'ab32.png';
+        img.style.opacity = '1';
+    }, 500);
+}
